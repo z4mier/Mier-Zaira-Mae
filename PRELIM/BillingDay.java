5f5be66185e0642a63c00ba1922f5be35d909cfa
z4mier <148032729+z4mier@users.noreply.github.com>
2023-10-25 06:07:16 +0100

    BillingDay.java

---
+++ PRELIM/BillingDay.java
@@ -0,0 +1,11 @@
+public class NumbersPrintIn
+{
+   public static void main(String[] args)
+   {
+      int billingDate = 5;
+      System.out.print("Bills are sent on day ");
+      System.out.print(billingDate);
+      System.out.println(" of the month");
+      System.out.println("Next bill: October " + billingDate);
+    }
+}
