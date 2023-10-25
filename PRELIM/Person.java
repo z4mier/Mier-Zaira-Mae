549a803f35eb1e55710d1feac3c52292dd6895da
z4mier <148032729+z4mier@users.noreply.github.com>
2023-10-25 05:54:29 +0100

    Person.java

---
+++ PRELIM/Person.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class Person {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private int age;
+    private String birthday;
+    private String address;
+
+    public Person() {
+        // Default constructor
+    }
+
+    public Person(String firstName, String middleName, String lastName, int age, String birthday, String address) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.age = age;
+        this.birthday = birthday;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", birthday='" + birthday + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("First Name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Middle Name: ");
+        String middleName = scanner.nextLine();
+        System.out.print("Last Name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+        System.out.print("Birthday: ");
+        String birthday = scanner.nextLine();
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+        
+        scanner.close();
+    }
+}
